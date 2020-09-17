15ab82fe227a659a9c1f237228cc4118c499c9b3
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-09-17 10:17:45 -0400

    working api call, and click function pulling zip code

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // zip code as determined by user
 var zipCode = $("#zip")
 var submit = $("#submitButton");
+
 submit.click(function(){
     var zip = zipCode.val();
-    console.log(zip);
-    callApi();
-});
-
-
-function callApi(){
-   // URL for openWeather API to determine coordinates, to be used in the hiking API
+    apiCall(zip);
+})
+function apiCall(zip){
+    // URL for openWeather API to determine coordinates, to be used in the hiking API
     var queryURLcoord = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=209f8c58871bc71410613a476769ea05`
 
     $.ajax({
@@ -29,9 +27,9 @@ function callApi(){
         var queryURL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=${max}&minLength=${minimumTrailLength}&key=200914730-ab7614e0ee0bfb0265f3fa4b3c4abc83`
 
         $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(hikingData){
+            url: queryURL,
+            method: "GET"
+        }).then(function(hikingData){
             console.log(hikingData);
                 
             for(i = 0 ; i < hikingData.trails.length; i++){
@@ -48,7 +46,6 @@ function callApi(){
                 //     });
                 
             };
-        
         }); 
-    };
-};
+    });
+}
